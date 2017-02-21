8f9655362f93699367269f18653250673f61b3d5
Nha Bui <bngnha@gmail.com>
2017-02-21 17:48:36 +0700

    implement notification server using socket

---
+++ notify.js
@@ -0,0 +1,13 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var Redis = require('ioredis');
+var redis = new Redis();
+
+redis.subscribe('notify');
+redis.on('message', function(channel, message){
+	console.log('Channel: ' + channel + ' message: ' + message);
+	io.emit(channel, message);
+});
+
+server.listen(9990);
