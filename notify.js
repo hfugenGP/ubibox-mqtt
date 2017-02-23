f457f152b341dfa2b0b72e35e706f14173e8ae66
Nha Bui <bngnha@gmail.com>
2017-02-23 09:52:52 +0700

    subscrible more channel to refresh new data

--- notify.js
+++ notify.js
@@ -5,6 +5,9 @@ var Redis = require('ioredis');
 var redis = new Redis();
 
 redis.subscribe('notify');
+redis.subscribe('application');
+redis.subscribe('device');
+redis.subscribe('event');
 redis.on('message', function(channel, message){
 	console.log('Channel: ' + channel + ' message: ' + message);
 	io.emit(channel, message);
