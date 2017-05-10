1caebcab3424b4f09137342d3229c82bde8533b1
Phong Vu <Phong Vu>
2017-05-10 13:49:45 +0700

    fix issue with hex encode

--- common.js
+++ common.js
@@ -18,7 +18,7 @@ Common.prototype.hexEncode = function(str) {
 
     var result = "";
     for (i = 0; i < str.length; i++) {
-        hex = str.charCodeAt(i).toString(16);
+        hex = str.substr(i, 1).toString(16);
         result += ("000" + hex).slice(-4);
     }
 
