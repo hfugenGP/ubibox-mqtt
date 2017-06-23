486eb3506246f2e0476573160db3cd72a9f70cee
phongvu <phongvu2508@gmail.com>
2017-06-23 13:11:59 +0700

    fix notify service

--- notify.js
+++ notify.js
@@ -1,7 +1,12 @@
 // var app = require('express')();
 // var server = require('http').Server(app);
 // var io = require('socket.io')(server);
-var io = require('socket.io').listen(9990);
+var server = require('http').createServer(function(req, res) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('hello world');
+    response.end();
+});
+var io = require('socket.io').listen(server);
 
 var Redis = require('ioredis');
 var redis = new Redis({ dropBufferSupport: true });
@@ -17,4 +22,4 @@ redis.on('message', function(channel, message) {
     io.emit(channel, message);
 });
 
-// server.listen(9990);
+server.listen(9990);
