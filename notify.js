e25b6033f85f830a88a756834c3e02f4d1a4d8b9
phongvu <phongvu2508@gmail.com>
2017-06-23 13:25:08 +0700

    clean up notify service

--- notify.js
+++ notify.js
@@ -1,16 +1,13 @@
-// var app = require('express')();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
+var Redis = require('ioredis');
+var io = require('socket.io');
+
 var server = require('http').createServer(function(req, res) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write('hello world');
     response.end();
 });
-var io = require('socket.io').listen(server);
 
-var Redis = require('ioredis');
 var redis = new Redis({ dropBufferSupport: true });
-
 redis.subscribe('notify');
 redis.subscribe('notifyUnread');
 redis.subscribe('application');
@@ -23,3 +20,4 @@ redis.on('message', function(channel, message) {
 });
 
 server.listen(9990);
+io.listen(server);
