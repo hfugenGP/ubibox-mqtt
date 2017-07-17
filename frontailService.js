e059fa2cbc8b025704533da48cdc035e5091bc22
phongvu <phongvu2508@gmail.com>
2017-07-17 15:46:01 +0700

    hide log url

--- frontailService.js
+++ frontailService.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 const config = require('./config/conf');
 
-exec("frontail " + config.logStream, function(error, stdout, stderr) {
+exec("frontail --ui-hide-topbar " + config.logStream, function(error, stdout, stderr) {
     if (error) console.log(error);
     if (stdout) console.log(stdout);
     if (stderr) console.log(stderr);
