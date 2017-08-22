3924188a748865ca4da7a61e8069e39dbd84a9d2
phongvu <phongvu2508@gmail.com>
2017-08-22 16:01:25 +0700

    implement zte notification service

---
+++ zteNotify.js
@@ -0,0 +1,17 @@
+var Redis = require('ioredis');
+
+var server = require('http').createServer(function(req, res) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('hello world');
+    response.end();
+});
+
+server.listen(9990);
+var io = require('socket.io').listen(server);
+
+var redis = new Redis({ dropBufferSupport: true });
+redis.subscribe('zteGPSData');
+redis.subscribe('zteDeviceResponse');
+redis.on('message', function(channel, message) {
+    io.emit(channel, message);
+});
