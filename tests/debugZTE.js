c36475442744a7500c1d95b541b4a1bbab3d2709
phongvu <phongvu2508@gmail.com>
2017-08-23 14:59:46 +0700

    add script to migrate gpsData

--- tests/debugZTE.js
+++ tests/debugZTE.js
@@ -2,7 +2,16 @@ const Common = require('../lib/common');
 const config = require('../config/conf');
 const ZTEDataService = require('../services/zteDataService');
 
-var zteDataService = new ZTEDataService();
+const f = require('util').format;
+const MongoClient = require('mongodb').MongoClient;
+
+var user = encodeURIComponent(config.zte.mongoUsername);
+var password = encodeURIComponent(config.zte.mongoPassword);
+
+// Connection URL
+var url = f(config.zte.mongoUrl, user, password, config.zte.mongoAuthMechanism);
+
+// var zteDataService = new ZTEDataService();
 
 // var hexData = "555500553836313437333033303036383836376ae06a9506407a68ae3a374b24dd73f66294257c920a437338c3a340b44b71aad492941611c3189b5ca08baebf6b6d67db1a6eca8d41e3ad39ec3d51f4a873c8aaaa";
 // var hexData = "5555005df9b865046e5f774c383631343733303330313437393335a0594f94147c478446935f644a9ad3484fbb573416e730c3b0bf2b7010c0b0e986658aed66c86b547b94e3ce56ee322a4b6c917a996ad1bf1c68057ee208bf72aaaa";
@@ -17,8 +26,19 @@ var zteDataService = new ZTEDataService();
 //     return;
 // }
 
-console.log('*****************************************************************');
+// console.log('*****************************************************************');
 
-var messageCallback = zteDataService.generateMessageToDevice(null, "", "", "0x02", { '0x0001': '1.6' });
+// var messageCallback = zteDataService.generateMessageToDevice(null, "", "", "0x02", { '0x0001': '1.6' });
 
-console.log('************************End data received************************');
+// console.log('************************End data received************************');
+
+MongoClient.connect(url, function(err, db) {
+    var trips = db.collection("Trips").find();
+
+    trips.forEach(function(trip) {
+        db.collection('GPSData').updateMany({ deviceId: trip.deviceId, gpsType: "routing", positionTime: { $gt: trip.ignitionOnTime, $lt: trip.ignitionOffTime }, }, { $set: { tripId: trip._id } }, {
+            upsert: true,
+            multi: true
+        });
+    }, this);
+});
